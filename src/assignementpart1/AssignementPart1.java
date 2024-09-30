d54e4d965200144f651a01a68beca19531011f66
orati <orati@OraMmusi>
2024-09-30 22:15:23 +0200

    Ninenth Commit

---
+++ src/assignementpart1/AssignementPart1.java
@@ -0,0 +1,27 @@
+/*
+Name: Oratile
+Surname: Mmusi
+Student No.: ST10312691
+Description: POE Part 1
+Due date: 30 September 2024
+
+ */
+package assignementpart1;
+
+/**
+ *
+ * @author orati
+ */
+public class AssignementPart1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Registration registration = new Registration ();
+        Loginn login = new Loginn (registration); 
+        login.setVisible (true);
+        // TODO code application logic here
+    }
+    
+}
